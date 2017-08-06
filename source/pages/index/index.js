76b72571d8c4f66a9da0cb6ca16e1a03cc66b6e2
bad4iz <bad4iz@gmail.com>
2017-08-06 15:54:38 +0400

    ssds

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -5,8 +5,6 @@ import 'normalize.css';
 
 import '../../components/shemeStrapping/schemeStrapping';
 
-
-
 import Cork from './components/Cork';
 import CoilTubings from './components/CoilTubings';
 import Tubes from './components/Tubes';
