1c4ccf659ba62756acb92638dd0aa5a8d2c8cde6
bad4iz <bad4iz@gmail.com>
2017-08-07 11:49:53 +0400

    анимация и логика закрытия 3 задвижки фонтанной арматуры

--- source/components/shemeStrapping/schemeStrapping.js
+++ source/components/shemeStrapping/schemeStrapping.js
@@ -1,19 +1,30 @@
 import './schemeStrapping.scss';
 
-var kvds =  [].slice.call(document.querySelectorAll('.kvd'));
+var kvds = [].slice.call(document.querySelectorAll('.kvd'));
 
-kvds.forEach(kvd =>{
-    kvd.addEventListener('click',()=>{
-         kvd.classList.toggle('kvdOf')
-    },false);
+kvds.forEach(kvd => {
+    kvd.addEventListener('click', () => {
+        kvd.classList.toggle('kvdOf')
+    }, false);
 });
 
 var valves = [].slice.call(document.querySelectorAll('.valve'));
 
-valves.forEach(valve =>{
-    valve.addEventListener('click',()=>{
-         valve.classList.toggle('valveOf')
-    },false);
+valves.forEach(valve => {
+    valve.addEventListener('click', () => {
+        valve.classList.toggle('valveOf');
+        // console.log(valve.className);
+        christmasTreeClosed3(valve);
+    }, false);
 });
 
-console.log('in schemeStrapping.js');
+
+// замыкание создание функции и вызов ее же
+var christmasTreeClosed3  = (() => {
+    var christmasTree3 =  [].slice.call(document.querySelectorAll('.christmasTree_3'));
+    return (valve) => {
+        if (valve.id === 'christmasTree_3') {
+            christmasTree3.forEach(item => item.classList.toggle('christmasTree_3_closed'))
+        }
+    }
+})();
