9d1ef0d0d669ac7c5a9024b0d3d1198850cccc44
bad4iz <bad4iz@gmail.com>
2017-08-10 13:59:39 +0400

    добавление / удаление давления в установках

--- source/components/shemeStrapping/schemeStrapping.js
+++ source/components/shemeStrapping/schemeStrapping.js
@@ -28,3 +28,17 @@ var christmasTreeClosed3  = (() => {
         }
     }
 })();
+
+// добавле удаление давления в установках
+let pressures = [].slice.call(document.querySelectorAll('.pressure '));
+// вывод в консоль
+let getPressuresItem = (el) => {
+  let parent = el.parentNode.parentNode.className;
+  let child = ~el.className.indexOf('buttonPlus')?1:0;
+
+  console.log('RivgCodeinterpreter(SetChenjPer([' + parent + ']////'+ child + ');)EndCode');
+};
+// сам обработчик
+pressures.forEach(pressure => {
+    [].slice.call(pressure.children).forEach(item => item.addEventListener('click', getPressuresItem.bind(null,item), false));
+});
