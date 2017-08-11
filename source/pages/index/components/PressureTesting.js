5735b81ced121943cecf45e139841f057d1e2621
bad4iz <bad4iz@gmail.com>
2017-08-11 08:40:42 +0400

    #15 опресовка фореач не работает

--- source/pages/index/components/PressureTesting.js
+++ source/pages/index/components/PressureTesting.js
@@ -1,7 +1,7 @@
 export default class PressureTesting {
   constructor() {
-    this.pressureTesting1 = document.querySelectorAll('.pressureTesting_1');
-    this.pressureTesting2 = document.querySelectorAll('.pressureTesting_2');
+    this.pressureTesting1 = [].slice.call(document.querySelectorAll('.pressureTesting_1'));
+    this.pressureTesting2 = [].slice.call(document.querySelectorAll('.pressureTesting_2'));
   }
 
   isClosed() {
@@ -12,6 +12,7 @@ export default class PressureTesting {
   // 1 до первой задвижке
   // 2 до второй задвижки
   // 3 полная промывка
+  //  любой другой сброс до нуля
   testing(number){
     switch (number) {
       case 3:
@@ -23,12 +24,12 @@ export default class PressureTesting {
         this.pressureTesting2.forEach(item => item.className ='pressureTesting_2 pressureTesting_2_2' );
         break;
       case 1:
-      this.pressureTesting1.forEach(item =>item.className ='pressureTesting_1' );
-      this.pressureTesting2.forEach(item => item.className ='pressureTesting_2 pressureTesting_2_1' );
+        this.pressureTesting1.forEach(item =>item.className ='pressureTesting_1' );
+        this.pressureTesting2.forEach(item => item.className ='pressureTesting_2 pressureTesting_2_1' );
         break;
       default:
-      this.pressureTesting1.forEach(item => item.className = 'pressureTesting_empty pressureTesting_1' );
-      this.pressureTesting2.forEach(item => item.className = 'pressureTesting_empty pressureTesting_2' );
+        this.pressureTesting1.forEach(item => item.className = 'pressureTesting_empty pressureTesting_1' );
+        this.pressureTesting2.forEach(item => item.className = 'pressureTesting_empty pressureTesting_2' );
     }
   }
 }
